df0b87a51b2f0b189f668e7b8093c78c317fc815
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-08 10:09:26 +0200

    tupple

---
+++ public/tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+// Tuple
+const user = ['James', 25, true];
+// const usr2: UserArr = ['Jane', false, 30];
+const usr2 = ['Jane', 30, true];
+user[0] = 'Mike';
+//# sourceMappingURL=tuple.js.map
---
+++ src/tuple.ts
@@ -0,0 +1,10 @@
+// Tuple
+
+type UserArr = [string, number, boolean];
+
+const user: [string, number, boolean] = ['James', 25, true];
+
+// const usr2: UserArr = ['Jane', false, 30];
+const usr2: UserArr = ['Jane', 30, true];
+
+user[0] = 'Mike';
