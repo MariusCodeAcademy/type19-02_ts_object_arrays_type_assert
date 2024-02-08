dd187b1d0e68c763e38fa674a8d8bb15515b4b3d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-08 10:31:42 +0200

    tuple bug

--- public/tuple.js
+++ public/tuple.js
@@ -4,4 +4,15 @@ const user = ['James', 25, true];
 // const usr2: UserArr = ['Jane', false, 30];
 const usr2 = ['Jane', 30, true];
 user[0] = 'Mike';
+// su tuple nereiketu daryti push, pop, shift unshift, splice ir kitu masyva keicianciu operaciju
+// user.push(50); // zinomas bugas
+console.log('user ===', user);
+const users = [
+    [1, 'james@email.com'],
+    [2, 'mike@email.com'],
+    [3, 'Jill@email.com'],
+    [4, 'Sebentautas@email.com'],
+];
+users.push([6, 'mail@mail.com']);
+console.log('users ===', users);
 //# sourceMappingURL=tuple.js.map
--- src/tuple.ts
+++ src/tuple.ts
@@ -8,3 +8,19 @@ const user: [string, number, boolean] = ['James', 25, true];
 const usr2: UserArr = ['Jane', 30, true];
 
 user[0] = 'Mike';
+
+// su tuple nereiketu daryti push, pop, shift unshift, splice ir kitu masyva keicianciu operaciju
+// user.push(50); // zinomas bugas
+
+console.log('user ===', user);
+
+const users: [number, string][] = [
+  [1, 'james@email.com'],
+  [2, 'mike@email.com'],
+  [3, 'Jill@email.com'],
+  [4, 'Sebentautas@email.com'],
+];
+
+users.push([6, 'mail@mail.com']);
+
+console.log('users ===', users);
