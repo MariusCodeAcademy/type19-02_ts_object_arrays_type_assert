36456222bda62a629a846de24ef11b9c338dd66e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-08 11:45:04 +0200

    sort

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
 import { people } from './data/persons.js';
 console.log('Hello from app.ts!');
-console.table(people);
+// console.table(people);
+// parasyti fn
+// gauna arggumentu people masyva ir atspaustina vardus
+function printNames(arr) {
+    const namesArr = arr.map((pObj) => pObj.name);
+    console.log('namesArr ===', namesArr);
+}
+printNames(people);
+// parasyti fn kuri isrikiuoja masyva pagal income ir grazina ji
+function sortByIncome(arr) {
+    const sorted = arr.sort((a, b) => a.income - b.income);
+    return sorted;
+}
+const rikiuotas = sortByIncome(people);
+function sortByNuberVal(arr, sortBy) {
+    const sorted = arr.sort((a, b) => a[sortBy] - b[sortBy]);
+    return sorted;
+}
+const rikiuotasPgl = sortByNuberVal(people, "age" /* NuberCol.age */);
+console.table(rikiuotasPgl);
+function sortByStringVal(arr, sortBy) {
+    const sorted = arr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+    return sorted;
+}
+const rikiuotasPglVarda = sortByStringVal(people, "name" /* StrCol.name */);
+console.table(rikiuotasPglVarda);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,52 @@
-import { people } from './data/persons.js';
+import { PersObj, people } from './data/persons.js';
 
 console.log('Hello from app.ts!');
 
-console.table(people);
+// console.table(people);
+
+// parasyti fn
+// gauna arggumentu people masyva ir atspaustina vardus
+
+function printNames(arr: PersObj[]): void {
+  const namesArr: string[] = arr.map((pObj) => pObj.name);
+  console.log('namesArr ===', namesArr);
+}
+
+printNames(people);
+
+// parasyti fn kuri isrikiuoja masyva pagal income ir grazina ji
+
+function sortByIncome(arr: PersObj[]): PersObj[] {
+  const sorted: PersObj[] = arr.sort((a, b) => a.income - b.income);
+  return sorted;
+}
+const rikiuotas = sortByIncome(people);
+// console.table(rikiuotas);
+
+// parasyti fn kuri isrikiuoja masyva pagal income ir grazina ji
+// argumentu paduosim stulpeli kuris gali buti tik age arba income
+
+const enum NuberCol {
+  age = 'age',
+  income = 'income',
+}
+
+function sortByNuberVal(arr: PersObj[], sortBy: 'age' | 'income'): PersObj[] {
+  const sorted: PersObj[] = arr.sort((a, b) => a[sortBy] - b[sortBy]);
+  return sorted;
+}
+const rikiuotasPgl = sortByNuberVal(people, NuberCol.age);
+console.table(rikiuotasPgl);
+
+const enum StrCol {
+  name = 'name',
+  surname = 'surname',
+  sex = 'sex',
+}
+
+function sortByStringVal(arr: PersObj[], sortBy: StrCol): PersObj[] {
+  const sorted: PersObj[] = arr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+  return sorted;
+}
+const rikiuotasPglVarda = sortByStringVal(people, StrCol.name);
+console.table(rikiuotasPglVarda);
